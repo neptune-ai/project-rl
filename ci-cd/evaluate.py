b0d3d14fc70529d2eb453853405784c8a88cdf7e
kamil-kaczmarek <kamil.kaczmarek@neptune.ml>
2021-06-30 16:45:28 +0200

    fix cicd

--- ci-cd/evaluate.py
+++ ci-cd/evaluate.py
@@ -1,6 +1,8 @@
+import os
 import neptune.new as neptune
 
 run = neptune.init(
+    api_token=os.getenv('NEPTUNE_API_TOKEN'),
     project="common/project-rl",
     name="evaluate",
     tags=["tmp"],
